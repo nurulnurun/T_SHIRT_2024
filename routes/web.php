015e0e73d3fcbb8ab6a1f5482b749c6316df9f1a
Nurul Haque <nurulhaque.engineer@gmail.com>
2024-01-16 10:03:18 +0600

    Laravel Installed and Backend Template Design both of Route New Create and controller and a blade file create.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+
+Route::get('/', [HomeController::class, 'master'])->name('admin.master');
+
